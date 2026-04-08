54198372aec5f5062cb353c29d799024af818ee0
suyog prasai <suyog224356@gmail.com>
2026-04-08 03:56:00 +0000

    updated architecture of the code:

--- microlane/models/lanenet2.py
+++ microlane/models/lanenet2.py
@@ -5,28 +5,15 @@ from typing import Tuple, List
 from microlane.schema.sample import Sample
 from microlane.schema.model_limbs import LaneNet2Input
 from microlane.schema.prediction import LanePrediction
-from utils import parse_config_utils
-
-
-CFG = parse_config_utils.lanenet_cfg
 
 class LaneNet2():
     
     def __init__(
         self,
-        weights_path,
-        with_lane_fit: bool = True,
-        
+        weights_path,        
         ):
         
         self.weights_path = weights_path
-        self.with_lane_fit = with_lane_fit
-
-        self._input_tensor = None
-        self._binary_seg_ret = None
-        self._instance_seg_ret = None
-        self._sess = None
-        self._postprocessor = None
         
         self._load_model()
              
@@ -34,7 +21,11 @@ class LaneNet2():
     
     def infer(self, image: LaneNet2Input) -> LanePrediction:
            
-        pass
+        return LanePrediction(
+            lanes=[],
+            confidence=[],
+            image_path="",
+        )
         
     
     def batch_infer(self, batch: List[LaneNet2Input]) -> List[LanePrediction]:
@@ -47,16 +38,4 @@ class LaneNet2():
     
     def _load_model(self):
         
-        self._input_tensor = tf.placeholder(
-            dtype=tf.float32, shape=[1, 256, 512, 3], name='input_tensor'
-        )
-        
-        
-        
-    
-    @staticmethod
-    def _minmax_scale(arr: np.ndarray) -> np.ndarray:
-        mn, mx = arr.min(), arr.max()
-        return (arr - mn) * 255.0 / (mx - mn)
-  
-    
+        pass
