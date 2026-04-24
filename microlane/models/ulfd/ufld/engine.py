9f16b618e85d3a4e5864a491782159b91fc0cc16
suyog prasai <suyog224356@gmail.com>
2026-04-24 14:21:32 +0000

    added stuf to the ufld pipeline

---
+++ microlane/models/ulfd/ufld/engine.py
@@ -0,0 +1,20 @@
+class UFLDEngine():
+    
+    def __init__(self, weights_path) -> None:
+        
+        self.weights_path = weights_path
+        
+    
+    def predict(self) -> None:
+        
+        pass
+    
+    
+    def close(self) -> None:
+        
+        pass
+    
+    
+    def _build_graph(self) -> None:
+        
+        pass
--- microlane/models/ulfd/ufld/evaluate.py
+++ microlane/models/ulfd/ufld/evaluate.py
@@ -1,6 +1,25 @@
+from schemas.api_schemas import Sample
+from schemas.api_schemas import ModelPrediction
+
+from helpers.preprocessing import PreProcessor
+from engine import UFLDEngine
 
 class UFLD():
     
     def __init__(self, weights_path) -> None:
         
-        self.weights_path = weights_path
+        self._engine = UFLDEngine(weights_path)
+
+    
+    def infer(self, picture: Sample ) -> ModelPrediction:
+        
+        pass
+    
+    def close(self):
+        self._engine.close()
+    
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *_):
+        self.close()
