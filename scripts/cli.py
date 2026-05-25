e0fc55b81d956b1c6dc9ad45c7a97414819beef9
suyog prasai <suyog224356@gmail.com>
2026-05-25 07:26:29 +0000

    the graphing stuff is complete with fn and fp as well

--- scripts/cli.py
+++ scripts/cli.py
@@ -2,6 +2,7 @@ import click
 
 from scripts.commands.evaluate import evaluate
 from scripts.commands.summarize import summarize
+from scripts.commands.comp import compare
 
 @click.group()
 def main():
@@ -10,3 +11,4 @@ def main():
 
 main.add_command(evaluate)
 main.add_command(summarize)
+main.add_command(compare)
